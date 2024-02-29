f885bb62b225564d09fb932481a0df7e4050fcc8
Hassaan Idrees <139614780+HassaanIK@users.noreply.github.com>
2024-02-29 03:28:40 -0800

    Add files via upload

---
+++ training.py
@@ -0,0 +1,39 @@
+import torch
+from model import model
+from data_preparing import train_dl, val_dl
+
+# Training loop
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.Adam):
+    history = []
+    optimizer = opt_func(model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-8, weight_decay=0.0, amsgrad=False
+)
+    for epoch in range(epochs):
+        # Training Phase
+        model.train()
+        train_losses = []
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+lr = 0.001
+num_epochs = 5
+opt_func = torch.optim.Adam
+
+# history = fit(num_epochs, lr, model, train_dl, val_dl, opt_func)
+
+
+# torch.save(model.state_dict(), 'facemask_model_statedict1.pth')
